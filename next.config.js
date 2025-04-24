13f0464a565c49256e479c2b1fefcabf12f4baee
Shrishail <shrishail@grexa.ai>
2025-04-25 00:33:57 +0530

    removed image optimisation

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
         const url = new URL(item);
